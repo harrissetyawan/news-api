9660d553b020aec99730b67942831b4d93a32aca
Harris Setyawan <harrisfm90@gmail.com>
2023-05-30 20:57:13 +0700

    Migration & Passport [Fixed]

--- database/migrations/2023_05_30_115235_create_logs_table.php
+++ database/migrations/2023_05_30_115235_create_logs_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('logs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->foreignId('news_id')->constrained('news');
+            $table->enum('action', ['completed', 'updated', 'deleted']);
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('logs');
     }
 };
